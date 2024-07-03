b63871983a21bef85e860db927c60589b207e80c
svtsv01 <svyatoslav_kushnarev@bk.ru>
2024-07-03 13:28:30 +0800

    GET was fixed

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 import express from "express";
-import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
