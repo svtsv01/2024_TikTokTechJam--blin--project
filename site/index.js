1e4f28febf3ea6ad3d925e48392481a32cc666c6
Petr Gizatullin <65227475+petrg01@users.noreply.github.com>
2024-08-07 21:29:27 +1000

    Finished prediction from video and website

--- site/index.js
+++ site/index.js
@@ -1,12 +1,30 @@
 import express from "express";
+import { exec } from 'child_process';
 
 const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.render("index.ejs");
+  res.render("index.ejs", { number:  null});
+});
+
+app.post('/extract-text', (req, res) => {
+  const link = req.body.link;
+  console.log('Link submitted:', link);
+  //res.render("index.ejs", { number:  2});
+  // Run the Python script with the link as an argument
+  exec(`python script.py "${link}"`, (error, stdout, stderr) => {
+      console.log(`Python script output: ${stdout}`);
+      if(!stdout){
+        res.render("index.ejs", { number:  -1});
+      } else{
+      res.render("index.ejs", { number:  `${stdout}`});
+      }
+  })
+  
 });
 
 app.listen(port, () => {
---
+++ site/test.py
@@ -0,0 +1,116 @@
+from pydub import AudioSegment
+import os
+from moviepy.editor import *
+from vosk import Model, KaldiRecognizer, SetLogLevel
+import wave
+import json
+import fnmatch
+import numpy as np
+import torch
+import torch.nn as nn
+from transformers import AutoModel, BertTokenizerFast
+from contextlib import contextmanager
+import sys, os
+import warnings
+warnings.filterwarnings("ignore")
+
+SetLogLevel(-1)
+
+@contextmanager
+def suppress_stdout():
+    with open(os.devnull, "w") as devnull:
+        old_stdout = sys.stdout
+        sys.stdout = devnull
+        try:  
+            yield
+        finally:
+            sys.stdout = old_stdout
+
+tt_link = 'https://www.tiktok.com/@quansuthegioi.7/video/7262952008817102088?q=Russia&t=1723011602686'
+
+with suppress_stdout():
+    #Load video from TikTok
+    import pyktok as pyk
+    pyk.specify_browser('chrome')
+    pyk.save_tiktok(tt_link, True)
+    for file in os.listdir('.'):
+        if fnmatch.fnmatch(file, '*@*'):
+            video_path = file
+            break
+    #Extract MP3 from video
+    video = VideoFileClip(video_path)
+    audio = video.audio
+    audio.write_audiofile("output_audio.mp3")
+    # Load MP3 file, convert to wav and set parameters
+    audio = AudioSegment.from_mp3("output_audio.mp3")
+    audio.export("output_audio.wav", format="wav")
+    audio = AudioSegment.from_wav("output_audio.wav")
+    audio = audio.set_channels(1)
+    audio = audio.set_frame_rate(16000)
+    audio.export("output_audio_mono.wav", format="wav")
+    model = Model('vosk-model-en-us-0.22')
+    rec = KaldiRecognizer(model, 16000)
+    # Open WAV file
+    wf = wave.open("output_audio_mono.wav", "rb")
+    # List to hold all text segments
+    transcribed_text_list = []
+    while True:
+        data = wf.readframes(4000)
+        if len(data) == 0:
+            break
+        if rec.AcceptWaveform(data):
+            result = json.loads(rec.Result())
+            transcribed_text_list.append(result['text'])
+    # Handle last part of audio
+    final_result = json.loads(rec.FinalResult())
+    transcribed_text_list.append(final_result['text'])
+    # Concatenate all text segments
+    complete_text = ' '.join(transcribed_text_list)
+    print(complete_text)
+    # Load data
+    test_text = [complete_text]
+    # Load BERT model and tokenizer
+    bert = AutoModel.from_pretrained('bert-base-uncased')
+    tokenizer = BertTokenizerFast.from_pretrained('bert-base-uncased')
+    device = torch.device("cpu")
+    # Tokenize and encode sequences
+    max_length = 128
+    tokens_test = tokenizer.batch_encode_plus(test_text, max_length=max_length, pad_to_max_length=True, truncation=True)
+    # Convert lists to tensors
+    test_seq = torch.tensor(tokens_test['input_ids'])
+    test_mask = torch.tensor(tokens_test['attention_mask'])
+    # Model definition
+    class BERT_Arch(nn.Module):
+        def __init__(self, bert):
+            super(BERT_Arch, self).__init__()
+            self.bert = bert
+            self.dropout = nn.Dropout(0.3)
+            self.relu = nn.ReLU()
+            self.fc1 = nn.Linear(768, 512)
+            self.fc2 = nn.Linear(512, 3)
+            self.softmax = nn.LogSoftmax(dim=1)
+        def forward(self, sent_id, mask):
+            _, cls_hs = self.bert(sent_id, attention_mask=mask, return_dict=False)
+            x = self.fc1(cls_hs)
+            x = self.relu(x)
+            x = self.dropout(x)
+            x = self.fc2(x)
+            x = self.softmax(x)
+            return x
+    model = BERT_Arch(bert)
+    model = model.to(device)
+    # Load best model and evaluate data
+    model.load_state_dict(torch.load('../saved_weights.pt', map_location=torch.device('cpu')))
+    with torch.no_grad():
+        preds = model(test_seq.to(device), test_mask.to(device))
+        preds = preds.detach().cpu().numpy()
+    preds = np.argmax(preds, axis=1)
+
+print(preds[0])
+#Remove the files
+video.close()
+wf.close()
+os.remove('output_audio.mp3')
+os.remove('output_audio.wav')
+os.remove('output_audio_mono.wav')
+os.remove(video_path)
